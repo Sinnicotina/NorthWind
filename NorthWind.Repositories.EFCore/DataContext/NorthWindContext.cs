44e9b1c6a3e44daa58e3d328018595a16e4ba6cb
Esteban <esteban98e31@gmail.com>
2023-10-10 13:03:05 -0500

    Creado el proyecto inicial

---
+++ NorthWind.Repositories.EFCore/DataContext/NorthWindContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthWind.Entities.POCOEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Repositories.EFCore.DataContext
+{
+    public class NorthWindContext : DbContext
+    {
+        public NorthWindContext(DbContextOptions
+            <NorthWindContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.Id)
+                .HasMaxLength(5)
+                .IsFixedLength();
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(40);
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(40);
+            modelBuilder.Entity<Order>()
+                .Property(o => o.CustomerId)
+                .IsRequired()
+                .HasMaxLength(5)
+                .IsFixedLength();
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ShipAddress).IsRequired()
+                .HasMaxLength(60);
+
+        }
+
+
+    }
+}
