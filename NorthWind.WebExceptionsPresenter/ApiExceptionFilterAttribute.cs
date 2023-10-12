2612893182125671eebb508066cc5101d447d274
Esteban <esteban98e31@gmail.com>
2023-10-12 17:37:57 -0500

    OrderController

---
+++ NorthWind.WebExceptionsPresenter/ApiExceptionFilterAttribute.cs
@@ -0,0 +1,40 @@
+﻿using MediatR.Pipeline;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.WebExceptionsPresenter
+{
+    public class ApiExceptionFilterAttribute: ExceptionFilterAttribute
+    {
+        readonly IDictionary<Type, IExeptionHandler>
+            ExceptionHandlers;
+
+        public ApiExceptionFilterAttribute(IDictionary<Type,
+            IExeptionHandler>
+            exceptionHandlers)=>ExceptionHandlers
+            =exceptionHandlers;
+
+        public override void OnException(ExceptionContext context)
+        {
+            Type ExceptionType= context.Exception.GetType();
+            if (ExceptionHandlers.ContainsKey(ExceptionType))
+            {
+                ExceptionHandlers[ExceptionType].Handle(context);
+            }
+            else
+            {
+                new ExceptionHandlerBase().SetResult(context, StatusCodes
+                    .Status500InternalServerError, "Ha ocurrido un error al procesar" +
+                    "la respuesta.","");
+            }
+            base.OnException(context);
+        }
+
+
+    }
+}
